a7f45275878e8e813d4feaf7b57b7ad8d5f0f2a6
Jenna Prasadi <jenna5734@gmail.com>
2020-09-03 22:50:17 -0700

    Initial commmit

---
+++ flixter/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flixter
+//
+//  Created by Jenna Prasadi on 9/2/20.
+//  Copyright © 2020 jenna5734. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
